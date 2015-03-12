3616757887ebbe59b70861dfafdac402b144550e
Vidur Maheshwari <vidurm09@gmail.com>
2015-03-12 15:46:51 -0700

    File imports and Basic Motor Config

--- main.c
+++ main.c
@@ -1,3 +1,22 @@
+#pragma config(Sensor, in1,    autoPot,        sensorPotentiometer)
+#pragma config(Sensor, in2,    posPot,         sensorPotentiometer)
+#pragma config(Sensor, dgtl1,  rDriveEncoder,  sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  lDriveEncoder,  sensorQuadEncoder)
+#pragma config(Sensor, dgtl5,  rLiftEncoder,   sensorQuadEncoder)
+#pragma config(Sensor, dgtl7,  lLiftEncoder,   sensorQuadEncoder)
+#pragma config(Sensor, dgtl9,  dumpSolenoid,   sensorDigitalOut)
+#pragma config(Sensor, dgtl11, liftDetectLeft, sensorTouch)
+#pragma config(Sensor, dgtl12, liftDetectRight, sensorTouch)
+#pragma config(Motor,  port1,           driveLB,       tmotorVex393_HBridge, openLoop, reversed)
+#pragma config(Motor,  port2,           intakeR,       tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port3,           intakeL,       tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port4,           driveLF,       tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port5,           driveRF,       tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port6,           liftLT,        tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port7,           liftLB,        tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port8,           liftRB,        tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port9,           liftRT,        tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port10,          driveRB,       tmotorVex393_HBridge, openLoop)
 
 #pragma platform(VEX)
 
@@ -7,6 +26,10 @@
 #pragma userControlDuration(120)
 
 #include "Vex_Competition_Includes.c"   //Main competition background code...do not modify!
+#include "drive.c"
+#include "arm.c"
+#include "general.c"
+#include "auton.c"
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //
